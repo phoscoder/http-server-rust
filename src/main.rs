9e5de5bfa1930f357bc2576014b84b847692e2b6
Victor (Takunda) Phos <victorphos1@gmail.com>
2026-04-19 01:52:43 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::Write;
 use std::io::Read;
+use std::thread;
 
 
 
@@ -44,7 +45,9 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_connection(stream);
+                thread::spawn(move || {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
